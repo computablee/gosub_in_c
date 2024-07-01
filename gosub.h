24dbbb6592413d2258ed4c3f5e3a57122beb9604
Phil Lane <phillipallenlane@gmail.com>
2024-06-30 23:14:55 -0500

    make stack size changeable at compile time

--- gosub.h
+++ gosub.h
@@ -1,6 +1,10 @@
 #include <stdint.h>
 
-void *(__gosub_stack[256]);
+#ifndef GOSUB_STACK_SIZE
+#define GOSUB_STACK_SIZE 256
+#endif
+
+void *(__gosub_stack[GOSUB_STACK_SIZE]);
 size_t __gosub_ptr = 0;
 
 #define __gosub__create__label(label, linenum) label ## linenum
