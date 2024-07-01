fdd7925e72848d677a3f63f97891e02c6d520336
Phil Lane <phillipallenlane@gmail.com>
2024-06-30 23:27:09 -0500

    tidy up, fix bug

--- gosub.h
+++ gosub.h
@@ -8,7 +8,7 @@ void *(__gosub_stack[GOSUB_STACK_SIZE]);
 size_t __gosub_ptr = 0;
 
 #define __gosub__create__label(label, linenum) label ## linenum
-#define __gosub__create__label_2(label, linenum) __gosub__create__label(ret_label, linenum)
+#define __gosub__create__label_2(label, linenum) __gosub__create__label(label, linenum)
 
-#define GOSUB(label) { __gosub_stack[__gosub_ptr++] = &&__gosub__create__label_2(ret_label, __LINE__); goto label; __gosub__create__label_2(ret_label, __LINE__): }
+#define GOSUB(label) { __gosub_stack[__gosub_ptr++] = &&__gosub__create__label_2(__ret_label, __LINE__); goto label; __gosub__create__label_2(__ret_label, __LINE__): }
 #define RETURN { void *ptr = (void*)__gosub_stack[--__gosub_ptr]; goto *ptr; }
